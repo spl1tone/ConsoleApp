3c48f27d9538750252401a0009b959cd55479fb6
spl1tOne <levonvlad144@gmail.com>
2023-06-19 23:15:54 +0300

    0:changed main method

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,6 +7,11 @@
             Console.WriteLine("Hello, World1");
             Console.WriteLine("Hello, World2");
             Console.WriteLine("Hello, World3");
+
+            first_class first_ = new first_class();
+
+            first_.Method("Vlad");
+
         }
     }
 }
