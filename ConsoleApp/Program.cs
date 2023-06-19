bf03c9371de2b4425a7c03793ab2e6a03afa9885
spl1tOne <levonvlad144@gmail.com>
2023-06-19 23:08:48 +0300

    0: new line added

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,9 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World1");
+            Console.WriteLine("Hello, World2");
+            Console.WriteLine("Hello, World3");
         }
     }
 }
